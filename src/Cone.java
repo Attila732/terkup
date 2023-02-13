cf30f0725c70bd1e2509a6649ad000caf31bd3fc
kovács Attila <kovacsattila478@gmail.com>
2023-02-13 15:40:26 +0100

    kezdés

---
+++ src/Cone.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Cone {
+    double calcVolume(double radius, double height){
+        double volume=(1.0/3.0)* 
+        Math.pow(radius,2)*Math.PI*height;
+        return volume;
+
+    }
+    
+}
